de65723fb6339fb18bd887fcdea491837047fdf6
bbrrrvsg <myeonghui1813@gmail.com>
2026-03-23 16:11:15 +0900

    Feat : MemberEntity , CompanyEntity 작성

--- src/main/java/com/green/backend/member/entity/Company.java
+++ src/main/java/com/green/backend/member/entity/Company.java
@@ -1,14 +1,33 @@
 package com.green.backend.member.entity;
 
+import com.green.backend.member.dto.MemberDTO;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder@Table(name = "company")
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int company_id;
+
+    @Column(nullable = false,length = 30)
+    private String company_name;
+
+    @Column(nullable = false, length = 30,unique = true)
+    private String business_number;
+
+    @OneToMany(mappedBy = "company")
+    @Builder.Default
+    @ToString.Exclude
+    private List<Member> members = new ArrayList<>();
 
 }
--- src/main/java/com/green/backend/member/entity/Member.java
+++ src/main/java/com/green/backend/member/entity/Member.java
@@ -8,7 +8,36 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder@Table(name = "member")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long member_id;
+
+    @Column(name = "login_id", nullable = false, unique = true, length = 30)
+    private String login_id;
+
+    @Column(name = "password", nullable = false, length = 30)
+    private String password;
+
+    @Column(name = "party_name", length = 30)
+    private String party_name;
+
+    @Column(name = "phone_number", nullable = false, length = 20)
+    private String phone_number;
+
+    @Column(name = "email", nullable = false, unique = true, length = 30)
+    private String email;
+
+    @Column(name = "address", length = 100)
+    private String address;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+
+    private Company company;
+
 
 }
