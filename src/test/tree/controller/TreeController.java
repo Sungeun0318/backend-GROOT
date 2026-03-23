0c726f3dbfb2fba232c568051c26f8ff47fe445b
Sungeun0318 <kimsungeun0318@naver.com>
2026-03-23 17:46:06 +0900

    Feat : .gitignore 수정 완료

--- src/test/tree/controller/TreeController.java
+++
@@ -1,9 +0,0 @@
-package com.green.backend.tree.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RestController
-@RequestMapping("/api/trees")
-public class TreeController {
-}
--- src/test/tree/service/TreeService.java
+++
@@ -1,7 +0,0 @@
-package com.green.backend.tree.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class TreeService {
-}
