eac631857602766443bea6fafae0de4580130c38
lth0330 <dlxogud555@gmail.com>
2026-03-23 18:06:49 +0900

    fit : ExpertReport.java

--- src/main/java/com/green/backend/expertreport/service/ExpertReportService.java
+++ src/main/java/com/green/backend/expertreport/service/ExpertReportService.java
@@ -1,10 +1,11 @@
 package com.green.backend.expertreport.service;
 
 import com.green.backend.expert.repository.ExpertRepository;
+import com.green.backend.expertreport.repository.ExpertReportRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExpertReportService {
 
-    private ExpertRepository expertRepository;
+    private ExpertReportRepository expertReportRepository;
 }
