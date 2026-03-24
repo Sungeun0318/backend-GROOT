78e0d584157e8742e20175851d1d53a3d897c728
dokyung253-lang <dokyung253@gmail.com>
2026-03-24 17:31:07 +0900

    Feat: 답사 신청 등록 controller / dto/ entity

--- src/main/java/com/green/backend/application/dto/ApplicationDTO.java
+++ src/main/java/com/green/backend/application/dto/ApplicationDTO.java
@@ -1,12 +1,36 @@
 package com.green.backend.application.dto;
 
+import com.green.backend.application.entity.Application;
+import com.green.backend.expert.entity.Expert;
+import com.green.backend.member.entity.Member;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@Builder
+@NoArgsConstructor @AllArgsConstructor
+@Builder @Data
 public class ApplicationDTO {
-
+    private Long detail_id;              // 답사번호
+    private Long member_id;           // 기업번호
+    private Integer expert_id;           // 전문가번호
+    private String location1;           // 위도
+    private String location2;           // 경도
+    private Integer times;              // 정기차수
+    private String surveyStatus;        // 상태 (신청/진행중/완료)
+    private LocalDateTime dueDate;      // 답사일
+    private String content;             // 신청내용
+    private String opinion;             // 의견
+    // baseTime
+    private String createDate;
+    private String updateDate;
+    // Dto -> Entity 변환
+    public Application toEntity( ){
+        return Application.builder()
+                .location1(location1) .location2(location2) .surveyStatus("신청")
+                .times(times) .dueDate(dueDate) .content(content) .opinion(opinion)
+                .build();
+    }
 }
--- src/main/java/com/green/backend/application/service/ApplicationService.java
+++ src/main/java/com/green/backend/application/service/ApplicationService.java
@@ -1,7 +1,15 @@
 package com.green.backend.application.service;
 
+import com.green.backend.application.dto.ApplicationDTO;
+import com.green.backend.member.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class ApplicationService {
+    private final MemberRepository memberRepository;
+
 }
