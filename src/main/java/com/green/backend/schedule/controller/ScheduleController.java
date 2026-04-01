505d63224b3a400b0f53f2285e0157c6cb79971f
dokyung253-lang <dokyung253@gmail.com>
2026-04-01 18:26:09 +0900

    Feat : schedule 기능 추가, 임도경

--- src/main/java/com/green/backend/schedule/controller/ScheduleController.java
+++ src/main/java/com/green/backend/schedule/controller/ScheduleController.java
@@ -1,11 +1,20 @@
 package com.green.backend.schedule.controller;
 
+import com.green.backend.expert.dto.ExpertDTO;
+import com.green.backend.expert.entity.Expert;
+import com.green.backend.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/schedule")
 public class ScheduleController {
+    public final ScheduleService scheduleService;
+    // (1) 전문가가 불가능한 일정 등록
+    @PostMapping("")
+    public ResponseEntity<?> enrollSchedule( @RequestHeader("Authorization")String token, @RequestBody ExpertDTO expertDTO ){
+        return ResponseEntity.ok(true);
+    }
 }
--- src/main/java/com/green/backend/schedule/service/ScheduleService.java
+++ src/main/java/com/green/backend/schedule/service/ScheduleService.java
@@ -1,11 +1,48 @@
 package com.green.backend.schedule.service;
 
+import com.green.backend.expert.dto.ExpertDTO;
+import com.green.backend.expert.repository.ExpertRepository;
+import com.green.backend.member.dto.LoginTokenDTO;
+import com.green.backend.schedule.dto.ScheduleDTO;
+import com.green.backend.schedule.entity.Schedule;
+import com.green.backend.schedule.repository.ScheduleRepository;
+import com.green.backend.util.JwtUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
+@Slf4j
 @Transactional
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
+    public final ScheduleRepository scheduleRepository;
+    public final ExpertRepository expertRepository;
+
+    // (1) 전문가가 불가능한 일정 등록하기
+    private boolean enrollSchedule(ScheduleDTO scheduleDTO){
+        try {
+            if (!expertRepository.existsById(scheduleDTO.getExpertId())) {
+                throw new IllegalArgumentException("존재하지 않는 ID입니다.");
+            }
+
+            if (scheduleDTO.getNotAvailable() == null || scheduleDTO.getScheduleState() == null) {
+                return false;
+            }
+            Schedule schedule = scheduleDTO.toEntity();
+            scheduleRepository.save(schedule);
+            return true;
+        }catch (Exception e){
+            log.error("일정등록 중 오류 발생 : {} ", e.getMessage());
+            return false;
+        }
+    }
+    //
+
+
 }
