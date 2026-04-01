6c8b779b2934696d48bfee4f8e8cf0f512582e9f
lth0330 <dlxogud555@gmail.com>
2026-04-01 14:49:25 +0900

    feat : main 가져옴

--- src/main/java/com/green/backend/expertreport/controller/ExpertReportController.java
+++ src/main/java/com/green/backend/expertreport/controller/ExpertReportController.java
@@ -59,6 +59,4 @@ public class ExpertReportController {
         List<ExpertReportDTO> result = expertReportService.getSurveyDetail(detailId);
         return ResponseEntity.ok(result);
     }
-
-
 }
