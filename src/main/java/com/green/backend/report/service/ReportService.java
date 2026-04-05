60965896fdf575173bb33edbdf14570c25aa3d18
lth0330 <dlxogud555@gmail.com>
2026-04-05 18:42:37 +0900

    feat : ReportDto preview 기능 추가(하는중)

--- src/main/java/com/green/backend/report/service/ReportService.java
+++ src/main/java/com/green/backend/report/service/ReportService.java
@@ -1,9 +1,24 @@
 package com.green.backend.report.service;
 
+import com.green.backend.certification.repository.CertificationRepository;
+import com.green.backend.report.dto.ReportPreviewDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ReportService {
+
+
+    private final CertificationRepository certificationRepository;
+
+
+
+
+
+    public ReportPreviewDTO preview(Long mid){
+
+
+
+    }
 }
