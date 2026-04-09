a9f29481c61b44b0e99c95116ac000c4dd09e2c9
lth0330 <dlxogud555@gmail.com>
2026-04-09 14:42:04 +0900

    feat : ExpertReportController - 토큰추가

--- src/main/java/com/green/backend/expertreport/controller/ExpertReportController.java
+++ src/main/java/com/green/backend/expertreport/controller/ExpertReportController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/expert-reports")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(value = "http://localhost:5173", allowCredentials = "true")
 public class ExpertReportController {
 
     private final ExpertReportService expertReportService;
